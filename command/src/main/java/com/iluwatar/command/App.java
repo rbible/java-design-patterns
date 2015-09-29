99ed3ad9e5640c8884078a48f0de55a56ddbbea6
rbible <rbible@qq.com>
2015-09-30 00:19:05 +0800

    command
    command

--- command/src/main/java/com/iluwatar/command/App.java
+++ command/src/main/java/com/iluwatar/command/App.java
@@ -1,5 +1,10 @@
 package com.iluwatar.command;
 
+import com.iluwatar.command.service.InvisibilitySpell;
+import com.iluwatar.command.service.ShrinkSpell;
+import com.iluwatar.command.service.Wizard;
+import com.iluwatar.command.target.Goblin;
+
 /**
  * 
  * In Command pattern actions are objects that can be executed and undone. 
@@ -20,28 +25,28 @@ package com.iluwatar.command;
  */
 public class App {
 
-	public static void main(String[] args) {
-		Wizard wizard = new Wizard();
-		Goblin goblin = new Goblin();
+    public static void main(String[] args) {
+        Wizard wizard = new Wizard();
+        Goblin goblin = new Goblin();
 
-		goblin.printStatus();
+        goblin.printStatus();
 
-		wizard.castSpell(new ShrinkSpell(), goblin);
-		goblin.printStatus();
+        wizard.castSpell(new ShrinkSpell(), goblin);
+        goblin.printStatus();
 
-		wizard.castSpell(new InvisibilitySpell(), goblin);
-		goblin.printStatus();
+        wizard.castSpell(new InvisibilitySpell(), goblin);
+        goblin.printStatus();
 
-		wizard.undoLastSpell();
-		goblin.printStatus();
+        wizard.undoLastSpell();
+        goblin.printStatus();
 
-		wizard.undoLastSpell();
-		goblin.printStatus();
+        wizard.undoLastSpell();
+        goblin.printStatus();
 
-		wizard.redoLastSpell();
-		goblin.printStatus();
+        wizard.redoLastSpell();
+        goblin.printStatus();
 
-		wizard.redoLastSpell();
-		goblin.printStatus();
-	}
+        wizard.redoLastSpell();
+        goblin.printStatus();
+    }
 }
